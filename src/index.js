bb81032fa9d3360d8395d22e2ee03651e1bed86a
nouvelle <nouvellenuts@gmail.com>
2022-11-04 00:29:07 +0900

    feat: スタイル修正

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Shop from "./Beverages.js";
 const main = document.getElementById("main");
 const submit = document.getElementById("submit");
+const reload = document.getElementById("reload");
 
 window.onload = () => {
   const shop = new Shop()
@@ -18,17 +19,23 @@ window.onload = () => {
     const isTarget = e.target.parentElement.classList.contains("drink-wrap__menu");
     if (!isTarget) return;
 
-    e.target.parentElement.classList.toggle("drink-active");
+    drinkBtnWrap.innerHTML = "";
+    drinkBtnWrap.appendChild(e.target.parentElement);
+    e.target.parentElement.classList.add("selected_drink");
+    
     const drinkId = e.target.parentElement.dataset.name;
     const targetDrink = drinkData[drinkId];
     shop.chooseBeverages(targetDrink.id);
 
-    const oldEle = document.getElementById("drink-wrap__customize");
+    const oldEle = document.getElementById("customize-wrap");
     if (oldEle) oldEle.remove();
 
     const customizeBtnWrap = document.createElement("div");
-    customizeBtnWrap.setAttribute("id", "drink-wrap__customize");
+    customizeBtnWrap.setAttribute("id", "customize-wrap");
     main.appendChild(customizeBtnWrap);
+
+    submit.classList.remove("disp_none");
+    reload.classList.remove("disp_none");
     
     targetDrink.type.forEach(type => {
       const customizeData = shop.getCustomizes(type);
@@ -46,19 +53,27 @@ window.onload = () => {
   
   // 3. 注文する
   submit.addEventListener("click", () => {
-    const rtn = shop.checkout();
+    const result = shop.checkout();
+
+    const finalWrap = document.createElement("div");
+    finalWrap.setAttribute("id", "final-wrap");
 
     const finalName = document.createElement("div");
-    finalName.setAttribute("id", "final_name");
-    finalName.textContent = rtn.beverage;
+    finalName.setAttribute("id", "final-wrap__name");
+    finalName.textContent = result.beverage;
 
     const finalPrice = document.createElement("div");
-    finalPrice.setAttribute("id", "final_price");
-    finalPrice.textContent = `${rtn.check}円`;
+    finalPrice.setAttribute("id", "final-wrap__price");
+    finalPrice.textContent = `${result.check}円`;
 
-    main.appendChild(finalName);
-    main.appendChild(finalPrice);
+    finalWrap.appendChild(finalName);
+    finalWrap.appendChild(finalPrice);
+    drinkBtnWrap.appendChild(finalWrap);
   });
+
+  reload.addEventListener("click", () => {
+    location.reload();
+  })
 }
 
 function createDrinkButton(items, parent) {
@@ -85,17 +100,17 @@ function createDrinkButton(items, parent) {
 function createCustomizeButton(items, targetDrink, parent) {
   const title = document.createElement("div");
   title.textContent = items.type;
-  title.setAttribute("class", "drink-wrap__title");
+  title.setAttribute("class", "customize-wrap__title");
   parent.appendChild(title);
   
   const btnWrap = document.createElement("div");
-  btnWrap.setAttribute("class", "drink-wrap__customize");
+  btnWrap.setAttribute("class", "customize-wrap__buttons");
   parent.appendChild(btnWrap);
 
   items.choices.forEach((item, i) => {
     const btn = document.createElement("button");
     btn.setAttribute("id", i);
-    btn.setAttribute("class", "drink-wrap__button");
+    btn.setAttribute("class", "customize-wrap__button");
 
     if (items.type === "size") {
       btn.dataset.price = targetDrink.price[i];
